2e7cbe487ba0a00457cb1328036ec87f523f6fd6
krexpex <64901989+krexpex@users.noreply.github.com>
2025-03-29 12:41:30 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Указываем, куда Vite должен сохранить скомпилированные файлы
+    emptyOutDir: true
+  },
+  root: './',  // Указываем корень проекта
+  publicDir: 'public',  // Указываем, где находятся статические файлы (index.html)
 })
