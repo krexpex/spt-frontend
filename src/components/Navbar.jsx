16b5e9e6a93fcd0168c39bddfb4573841ae5d04f
krexpex <64901989+krexpex@users.noreply.github.com>
2025-03-29 14:30:28 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 text-white p-4 mb-4 shadow-lg flex justify-between">
+    <nav className="bg-blue-600 text-white p-4 shadow-lg flex justify-between">
       <div className="font-bold text-xl">SPT WebApp</div>
       <div className="space-x-4">
-        <Link to="/" className="hover:text-gray-200">Главная</Link>
-        <Link to="/catalog" className="hover:text-gray-200">Каталог</Link>
-        <Link to="/profile" className="hover:text-gray-200">Профиль</Link>
-        <Link to="/cart" className="hover:text-gray-200">Корзина</Link>
-        <Link to="/history" className="hover:text-gray-200">История</Link>
+        <Link to="/">Главная</Link>
+        <Link to="/catalog">Каталог</Link>
+        <Link to="/profile">Профиль</Link>
+        <Link to="/cart">Корзина</Link>
+        <Link to="/history">История</Link>
       </div>
     </nav>
   );
