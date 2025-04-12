1c7ff6e0889c76efd8f4321201deff09a0a629ed
Valentins <Filipovica@kiber.local>
2025-04-12 23:00:40 +0300

    fix3.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-// src/App.jsx
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import NewsFeed from './components/NewsFeed';
 import Home from './components/Home';
@@ -9,22 +6,21 @@ import Profile from './components/Profile';
 import Catalog from './components/Catalog';
 import Cart from './components/Cart';
 import History from './components/History';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-100">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/news" element={<NewsFeed />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/catalog" element={<Catalog />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/history" element={<History />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<NewsFeed />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </div>
   );
 };
 
