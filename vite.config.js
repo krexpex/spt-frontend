2bda0a6db0d6b6b073b1fe02a1ed72f7ee117407
Valentins <Filipovica@kiber.local>
2025-04-12 22:41:34 +0300

    fix3

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: 'index.html'  // Важно! Здесь указано именно 'index.html' без указания пути.
+      '@': path.resolve(__dirname, './src')
     }
   },
-});
+  build: {
+    sourcemap: true, // Включаем sourcemap для дебага в продакшне
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173, // локальный порт, можно поменять
+    open: true
+  }
+})
